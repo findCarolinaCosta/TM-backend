4080947376d2d2345b07a3b885cdd6578d76fb3f
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-05-20 03:36:42 -0300

    fix: task model

--- src/models/prisma/PrismaTaskModel.ts
+++ src/models/prisma/PrismaTaskModel.ts
@@ -1,42 +1,67 @@
-import { prisma } from "../../prisma";
-import { ITaskTaskModel, ICreateTask, IUpdateTask } from "../ITaskModel";
+import { prisma } from '../../prisma';
+import { ITaskTaskModel, ICreateTask, IUpdateTask } from '../ITaskModel';
+
+interface ITask {
+  id: string;
+  task: string;
+  status: number;
+  createdAt: Date;
+}
+
+interface IUserTask {
+  userId: string;
+  taskId: string;
+}
 
 export class PrismaTaskModel implements ITaskTaskModel {
-  async create({ task, tags }: ICreateTask) {
-    await prisma.task.create({
+  async create({ task, status }: ICreateTask) {
+    const newTask: ITask | string | null = await prisma.task.create({
       data: {
         task,
-        tags,
+        status,
       },
     });
+
+    return newTask;
   }
 
   async readOne(id: string) {
-    await prisma.task.findMany({
+    const task: ITask[] = await prisma.task.findMany({
       where: { id },
     });
+
+    return task;
   }
 
-  async readMany(userId: string) {
-    await prisma.userTask.findMany({
-      where: { userId },
+  async readMany() {
+    const tasks: (IUserTask & {
+      tasks: ITask | null;
+    })[] = await prisma.userTask.findMany({
       include: { tasks: true },
     });
+
+    return tasks;
   }
 
-  async update({ id, task, tags }: IUpdateTask) {
-    await prisma.task.update({
+  async update({ id, task, status }: IUpdateTask) {
+    const updatedTask: ITask = await prisma.task.update({
       where: { id },
       data: {
         task,
-        tags,
+        status,
       },
     });
+
+    return updatedTask;
   }
 
   async delete(id: string) {
     await prisma.task.delete({
       where: { id },
+    });
+
+    await prisma.userTask.delete({
+      where: { taskId: id },
     });
   }
 }
