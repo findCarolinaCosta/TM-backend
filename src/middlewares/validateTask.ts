caf34470c1314c7e6dcf47f7b2b99db6316c3f32
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-05-20 03:41:55 -0300

    feat: implement task information validation middleware

---
+++ src/middlewares/validateTask.ts
@@ -0,0 +1,41 @@
+import { Request, Response, NextFunction } from 'express';
+import { taskCreate, taskUpdate } from './../schemas/taskSchema';
+import validateSchema from '../utils/validateSchema';
+
+interface ICreateTask {
+  userId?: string;
+  task: string;
+  status: number;
+}
+
+interface IUpdateTask {
+  id: string;
+  task: string;
+  status: number;
+}
+
+export class ValidateTask {
+  public async execute(req: Request, res: Response, next: NextFunction) {
+    const method: string = req.route.stack[0].method;
+    const obj = req.body;
+
+    const validStatus = ![0, 1, 2].includes(obj.status);
+
+    if (validStatus) {
+      return res.status(400).json({
+        message: 'Status should be 0 for pending, 1 in progress, 2 ready',
+      });
+    }
+
+    const error =
+      method === 'post'
+        ? validateSchema<ICreateTask>(obj, taskCreate)
+        : validateSchema<IUpdateTask>(obj, taskUpdate);
+
+    if (error) {
+      return res.status(error.status || 400).json({ message: error.message });
+    }
+
+    next();
+  }
+}
